b70f86803a3d766b4faa84117a1e5fa83fa79ab7
Jay200123 <kenken.ayonon@gmail.com>
2024-10-10 22:28:45 +0800

    Add Custom types

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log('Hello, world!');
+type Name = string;
+type Age = number;  
+type Student = boolean;
+
+const fname: Name = 'John';
+const age: Age = 25;
+const isStudent: Student = true;    
+
+console.log(fname, age, isStudent); 
