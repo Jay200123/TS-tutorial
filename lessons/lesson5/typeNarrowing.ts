c14a1d3cdd52094de1ef5fc606a65d61499dda82
Jay200123 <kenken.ayonon@gmail.com>
2024-10-12 22:03:58 +0800

    Add typeNarrowing

---
+++ lessons/lesson5/typeNarrowing.ts
@@ -0,0 +1,10 @@
+// function findPizzaDetails(identifier: number | string){
+//     if(typeof identifier === 'number'){
+//         return menu.find(pizza => pizza._id === identifier);
+//     }else{
+//         return menu.find(pizza => pizza.name.toLowerCase() === identifier.toLowerCase());
+//     }
+// }
+
+// const pizzaDetails = findPizzaDetails('Hawaiian');
+// console.log(pizzaDetails);  
