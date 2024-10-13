3ed90afd249ebdf559e4151924218702972fdd66
Jay200123 <kenken.ayonon@gmail.com>
2024-10-13 21:59:59 +0800

    Add dev.js

---
+++ dev.js
@@ -0,0 +1,16 @@
+let str = "12";
+str = +str;
+
+console.log(str);
+console.log(typeof str);
+
+let nr = "1";
+let x = (nr === 1);
+let y = (nr == 1);
+let z = (nr = 1);
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+console.log(3 * 4 > 20 - 15);
