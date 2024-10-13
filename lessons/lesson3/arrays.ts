bdd6ab16389408583a4d744ed97eeb87a602a2e6
Jay200123 <kenken.ayonon@gmail.com>
2024-10-13 22:00:36 +0800

    Refactor array

--- lessons/lesson3/arrays.ts
+++ lessons/lesson3/arrays.ts
@@ -20,3 +20,65 @@ function addItemToCart(product: Product) {
 
 addItemToCart({ name: 'Milk', price: 2.99 });
 console.log(items); 
+
+type info = {
+    block: number,
+    lot: number,
+    street: string,
+    city: string,
+}
+type Employee = {
+    id: number, 
+    name: string,
+    age: number,
+    isStudent: boolean,
+    address?: info,
+}
+
+type EmployeeList = Array<Employee>;
+const employees: EmployeeList = [
+    {
+        id: 1,
+        name: "John",
+        age: 25,
+        isStudent: true,
+        address: { block: 1, lot: 2, street: "Main St", city: "New York" },
+    },
+    {
+        id: 2,
+        name: "Jane",
+        age: 30,
+        isStudent: false,
+        address: { block: 2, lot: 3, street: "Broadway", city: "New York" },
+    },
+    {
+        id: 3,
+        name: "Doe",
+        age: 35,
+        isStudent: true,
+        address: { block: 3, lot: 4, street: "Wall St", city: "New York" },
+    },
+];
+
+
+const findOne = (id: number) => {   
+    return employees.find((e) => e.id === id);  // for single object    
+}
+
+const employee = findOne(1);
+console.log(employee);
+
+const getIds = employees?.map((e) => e?.id);
+console.log(getIds);
+
+const findUsers = (Id: Array<number>)=>{
+    return employees?.filter((e) => Id.includes(e.id));  // for multiple objects    
+};
+
+const employeeResult = findUsers(getIds);
+console.log(employeeResult);
+
+let n = 2;
+console.log(n **= 3);
+
+
