eef13430f45172e3d8b17f155552a44c9a22af56
Jay200123 <kenken.ayonon@gmail.com>
2024-10-16 21:51:20 +0800

    Add utility lesson

---
+++ lessons/lesson6/utility.ts
@@ -0,0 +1,52 @@
+let studentId = 1;
+
+type Student = {
+    id: number,
+    name: string,
+    age: number,
+    grade: number,  
+}
+
+type Updates = {
+    id?: number,
+    name?: string,
+    age?: number,
+    grade?: number,      
+}
+
+type Attendance = Array<Student>;
+const students: Attendance = [
+    {
+        id: studentId++, 
+        name: "John",
+        age: 12,
+        grade: 6
+    },
+    {
+        id: studentId++, 
+        name: "Jane",
+        age: 13,
+        grade: 7
+    },
+    {
+        id: studentId++, 
+        name: "Doe",
+        age: 14,
+        grade: 8
+    }
+];
+
+function updateStudent(id: number, updates: Updates): Student | undefined {	
+    const data = students.find((s) => s.id === id);
+
+    const updatedStudent = Object.assign(data, updates);
+    return updatedStudent;
+}
+
+const updateData = {
+    age: 15,
+    grade: 9        
+}
+
+const studentResult = updateStudent(1, updateData);
+console.log(studentResult);
