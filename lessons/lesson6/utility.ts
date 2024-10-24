db0c9f0ede3cef6cbd19504493a7dd92a1825226
Jay200123 <kenken.ayonon@gmail.com>
2024-10-24 20:41:33 +0800

    Refactor utility

--- lessons/lesson6/utility.ts
+++ lessons/lesson6/utility.ts
@@ -4,34 +4,34 @@ type Student = {
     id: number,
     name: string,
     age: number,
-    grade: number,  
+    grade: number,
 }
 
-type Updates =  Partial<Student>;    
+type Updates = Partial<Student>;
 
 type Attendance = Array<Student>;
 const students: Attendance = [
     {
-        id: studentId++, 
+        id: studentId++,
         name: "John",
         age: 12,
         grade: 6
     },
     {
-        id: studentId++, 
+        id: studentId++,
         name: "Jane",
         age: 13,
         grade: 7
     },
     {
-        id: studentId++, 
+        id: studentId++,
         name: "Doe",
         age: 14,
         grade: 8
     }
 ];
 
-function updateStudent(id: number, updates: Updates): Student | undefined {	
+function updateStudent(id: number, updates: Updates): Student | undefined {
     const data = students.find((s) => s.id === id);
 
     const updatedStudent = Object.assign(data, updates);
@@ -40,8 +40,29 @@ function updateStudent(id: number, updates: Updates): Student | undefined {
 
 const updateData = {
     age: 15,
-    grade: 9        
+    grade: 9
 }
 
 const studentResult = updateStudent(1, updateData);
 console.log(studentResult);
+
+
+function addNewStudent(newUser: any): Student | undefined {
+    const users: Student = {
+        ...newUser
+    };
+
+    students.push(users);
+    return users;
+}
+
+const student = {
+    id: studentId++,
+    name: "Test",
+    age: 14,
+    grade: 8
+}
+
+addNewStudent(student); 
+
+// const test = {...student};  
