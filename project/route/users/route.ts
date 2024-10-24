b354a191c9247a7349e2c157a9dba3b2400b172b
Jay200123 <kenken.ayonon@gmail.com>
2024-10-24 20:44:01 +0800

    Add user route

---
+++ project/route/users/route.ts
@@ -0,0 +1,39 @@
+import userController from "./controller";
+import { METHOD, PATH } from "../../constants/index";
+import { Router, Route } from "../../interface/middleware";
+
+const router = Router();
+
+
+const userRoutes: Route[] = [
+    {
+        method: METHOD.GET as keyof Router,
+        path: PATH.USERS,
+        handler: userController.getAllUsers
+    },
+    {
+        method: METHOD.GET as keyof Router,
+        path: PATH.USER_ID,
+        handler: userController.getUserById
+    },
+    {
+        method: METHOD.POST as keyof Router,
+        path: PATH.USERS,
+        handler: userController.createUser
+    },
+    {
+        method: METHOD.PATCH as keyof Router,
+        path: PATH.EDIT_USER_ID,
+        handler: userController.updateUser
+    },
+    {
+        method: METHOD.DELETE as keyof Router,
+        path: PATH.USER_ID,
+        handler: userController.deleteUser
+    }
+];
+
+userRoutes.forEach((route) => {
+    const { method, path = "", handler } = route;
+    router[method as any](path, handler);
+});
