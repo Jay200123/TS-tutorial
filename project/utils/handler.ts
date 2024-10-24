764af02438d34a1b87b2b225d16583a720afd531
Jay200123 <kenken.ayonon@gmail.com>
2024-10-24 20:58:06 +0800

    Add handler

---
+++ project/utils/handler.ts
@@ -0,0 +1,36 @@
+import { STATUSCODE } from "../constants/index";
+import { Response } from 'express';
+
+class ErrorHandler extends Error {
+    public statusCode: number;
+
+    constructor(message: string, statusCode: number = STATUSCODE.BAD_REQUEST) {
+        super(message);
+        this.statusCode = statusCode;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+
+    toJSON() {
+        return {
+            success: false,
+            error: {
+                message: this.message,
+            },
+        };
+    }
+}
+
+const SuccessHandler = (res: Response, message: string, details: any) => {
+    res.status(STATUSCODE.SUCCESS).json({
+        success: true,
+        message: message,
+        details: details,
+    });
+};
+
+
+export {
+    ErrorHandler,
+    SuccessHandler
+} 
