759cf46c006cf74da9a3c23dabdc56aaeea5ee96
Jay200123 <kenken.ayonon@gmail.com>
2024-10-27 21:35:02 +0800

    Add imageUpload file

---
+++ project/utils/imageUpload.ts
@@ -0,0 +1,43 @@
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import { v4 as uuidv4 } from 'uuid';
+import multer from 'multer';
+import { cloudinary } from '../config/index';
+import { Image } from '../interface';
+
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: async (req: Express.Request, file: Express.Multer.File) => ({
+    folder: "cloudinayImages",
+    public_id: `${file.originalname.replace(/\.[^/.]+$/, "")}-${uuidv4()}`,
+  }),
+});
+
+const upload = multer({ storage });
+
+const uploadImage = async (files: Express.Multer.File[], oldImagePublicIds: string[]): Promise<Image[]> => {
+  if (!files || !Array.isArray(files)) return [];
+
+
+  for (const publicId of oldImagePublicIds) {
+    if (publicId) await cloudinary.uploader.destroy(publicId);
+  }
+
+  const uploadPromises = files.map(file =>
+    cloudinary.uploader
+      .upload(file.path, { public_id: file.filename })
+      .then(result => ({
+        public_id: result.public_id,
+        url: result.secure_url,
+        originalname: file.originalname,
+      }))
+  );
+
+  return Promise.all(uploadPromises);
+};
+
+export {
+  upload,
+  uploadImage,
+}
+
