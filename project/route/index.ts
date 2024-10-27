7986d06704e4c5fde079ecf14eced5b5d34a30a8
Jay200123 <kenken.ayonon@gmail.com>
2024-10-27 12:45:55 +0800

    Refactor route  index file

--- project/route/index.ts
+++ project/route/index.ts
@@ -1,24 +1,2 @@
-import { Router } from "../interface";
-import userRoutes from "./users/route";
-import { PATH } from "../constants";
-
-
-const router = Router();
-
-interface Route {
-    path: string | string[];
-    route: Router
-}
-
-const routerList: Route[] = [
-    {
-        path: PATH.USERS,
-        route: userRoutes
-    }
-]
-
-routerList.map(routes => {
-    const { path, route } = routes
-    router.use(path, route)
-})
-export default router
+export { default as users } from "./users/route";
+export { default as products } from "./product/route";
