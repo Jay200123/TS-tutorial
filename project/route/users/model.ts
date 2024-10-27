df04ca6dbef8087a3fae4c9b27c2216e18b499f9
Jay200123 <kenken.ayonon@gmail.com>
2024-10-27 21:39:34 +0800

    Add image

--- project/route/users/model.ts
+++ project/route/users/model.ts
@@ -13,7 +13,14 @@ const userSchema: Schema<IUser> = new Schema({
     section: {
         type: String,
         required: true
-    }
+    },
+    image: [
+        {
+            public_id: String,
+            url: String,
+            originalname: String
+        }
+    ],
 });
 
 const User = model<IUser>(RESOURCE.USER, userSchema);
