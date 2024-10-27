4fb4790b8f9745729facd3e6be931a8e1092fe4d
Jay200123 <kenken.ayonon@gmail.com>
2024-10-27 12:46:41 +0800

    Refactor userController

--- project/route/users/controller.ts
+++ project/route/users/controller.ts
@@ -1,26 +1,26 @@
-import service from './service';
+import * as userService from './service';
 import { ErrorHandler, SuccessHandler } from '../../utils';
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, Response } from '../../interface/index';
 
 
 const getAllUsers = async (req: Request, res: Response) => {
-    const data = await service.getAll();
+    const data = await userService.getAll();
     return !data ? new ErrorHandler('No data found', 404) : SuccessHandler(res, "Data Found", data);
 };
 
 const getUserById = async (req: Request, res: Response) => {
-    const data = await service.getById(req.params.id);
+    const data = await userService.getById(req.params.id);
     return !data ? new ErrorHandler('No data found', 404) : SuccessHandler(res, "Data Found", data);
 }
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
-    const data = await service.create(req.body);
+    const data = await userService.create(req.body);
     return SuccessHandler(res, "Data Created", data);
 }
 
 
 const updateUser = async (req: Request, res: Response, next: NextFunction) => {
-    const data = await service.updateById(
+    const data = await userService.updateById(
         req.params.id,
         {
             ...req.body
@@ -32,7 +32,7 @@ const updateUser = async (req: Request, res: Response, next: NextFunction) => {
 
 
 const deleteUser = async (req: Request, res: Response) => {
-    const data = await service.deleteById(req.params.id);
+    const data = await userService.deleteById(req.params.id);
     return !data ? new ErrorHandler('No data found', 404) : SuccessHandler(res, "Data Deleted", data);
 }
 
