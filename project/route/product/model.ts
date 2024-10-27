24d6bbe93455c113f577bfecdd30a070841c1d20
Jay200123 <kenken.ayonon@gmail.com>
2024-10-27 12:46:15 +0800

    Add product model schema

---
+++ project/route/product/model.ts
@@ -0,0 +1,25 @@
+import { IProduct, Schema, model } from "../../interface";
+import { RESOURCE } from "../../constants";
+
+const productSchema: Schema<IProduct> = new Schema({
+    product_name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+
+})
+
+const Product = model<IProduct>(RESOURCE.PRODUCT, productSchema);
+export default Product
