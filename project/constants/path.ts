422b560f087cd730e02eda814c4889ea284cfd5d
Jay200123 <kenken.ayonon@gmail.com>
2024-10-27 12:44:39 +0800

    Add Product path constants

--- project/constants/path.ts
+++ project/constants/path.ts
@@ -1,6 +1,9 @@
 export const PATH = {
     USERS: "/users",
     USER_ID: "/user/:id",
-    EDIT_USER_ID:"/user/edit/:id"
+    EDIT_USER_ID: "/user/edit/:id",
+    PRODUCTS: "/products",
+    PRODUCT_ID: "/prodcut/:id",
+    EDIT_PRODUCT_ID: "/product/edit/:id"
 
 }
