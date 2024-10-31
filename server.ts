1a082b93372cc7e11fa47a028e1184c313ff4795
Jay200123 <kenken.ayonon@gmail.com>
2024-10-31 22:43:54 +0800

    Refactor server.ts

--- server.ts
+++ server.ts
@@ -2,23 +2,24 @@ import { globalEnvironment, connectDB } from "./project/config";
 import { Request, Response } from "./project/interface";
 import mongoose from "mongoose";
 import express from "express";
-import { users, products } from "./project/route";  
+import { users, products, auth } from "./project/route";  
+import { upload } from "./project/utils";
+import cookieParser from "cookie-parser"; 
 
 globalEnvironment();
 connectDB();
 const app = express();
 
-
+app.use(upload.array('image'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser());
-// app.use(compression()); 
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World");
 });
 
-app.use("/api/v1/", users, products);
+app.use("/api/v1/", auth, users, products);
 
 app.use((req: Request, res: Response) => {
     res.status(404).send("Not Found");
