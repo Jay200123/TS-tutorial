939f65e3b1de0399bb6db563cf079943ac3699b8
Jay200123 <kenken.ayonon@gmail.com>
2024-10-31 22:47:29 +0800

    Add authController

---
+++ project/route/authentication/controller.ts
@@ -0,0 +1,43 @@
+import * as userService from "../users/service";
+import { ErrorHandler, SuccessHandler, uploadImage, hashPassword } from "../../utils";
+import { Request, Response, NextFunction } from "../../interface";
+import bcrypt from "bcrypt";
+import { generateToken } from "../../middleware";
+import mongoose from "mongoose";
+
+
+const register = async (req: Request, res: Response, next: NextFunction) => {
+    const image = await uploadImage(req.files as Express.Multer.File[], []);
+    const password = await hashPassword(req.body.password);
+
+    const data = await userService.create(
+        {
+            ...req.body,
+            image: image,
+            password: password,
+        }
+    );
+
+    return SuccessHandler(res, "Data Created", data);
+}
+
+const login = async (req: Request, res: Response, next: NextFunction) => {
+    const data = await userService.getByEmail(req.body.email);
+
+    if (!data) {
+        throw new ErrorHandler("User Email not found");
+    }
+
+    const isPasswordMatch = await bcrypt.compare(req.body.password, data.password);
+    if (!isPasswordMatch) {
+        return next(new ErrorHandler('Wrong password', 401));
+    }
+
+    const token = generateToken({ _id: data._id as mongoose.Types.ObjectId });
+    SuccessHandler(res, "Login Success", data, token);
+}
+
+export default {
+    login,
+    register
+}
