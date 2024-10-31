a4b8fb16523b4f9c180a12de7f6183acbf86fb43
Jay200123 <kenken.ayonon@gmail.com>
2024-10-31 22:48:12 +0800

    Add verifyToken to userRoutes

--- project/route/users/route.ts
+++ project/route/users/route.ts
@@ -1,6 +1,7 @@
 import userController from "./controller";
 import { METHOD, PATH } from "../../constants/index";
 import { Router, Route } from "../../interface/type";
+import { verifyToken } from "../../middleware/verifyToken";
 
 const router = Router();
 
@@ -8,32 +9,31 @@ const userRoutes: Route[] = [
     {
         method: METHOD.GET as keyof Router,
         path: PATH.USERS,
+        middleware: [verifyToken], 
         handler: userController.getAllUsers
     },
     {
         method: METHOD.GET as keyof Router,
         path: PATH.USER_ID,
+        middleware: [verifyToken],
         handler: userController.getUserById
-    },
-    {
-        method: METHOD.POST as keyof Router,
-        path: PATH.USERS,
-        handler: userController.createUser
     },
     {
         method: METHOD.PATCH as keyof Router,
         path: PATH.EDIT_USER_ID,
+        middleware: [verifyToken],
         handler: userController.updateUser
     },
     {
         method: METHOD.DELETE as keyof Router,
         path: PATH.USER_ID,
+        middleware: [verifyToken],
         handler: userController.deleteUser
     }
 ];
 
 userRoutes.forEach((route) => {
-    const { method, path = "", handler } = route;
+    const { method, path, handler } = route;
     router[method as any](path, handler);
 });
 
