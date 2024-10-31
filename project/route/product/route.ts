4b74c4b5d31f9a02b890aa11a24574bcab549331
Jay200123 <kenken.ayonon@gmail.com>
2024-10-31 22:47:44 +0800

    Add verifyToken

--- project/route/product/route.ts
+++ project/route/product/route.ts
@@ -1,6 +1,7 @@
 import productController from "./controller";
 import { METHOD, PATH } from "../../constants";
 import { Route, Router } from "../../interface";
+import { verifyToken } from "../../middleware/verifyToken";
 
 const router = Router();
 
@@ -8,32 +9,37 @@ const productRoutes: Route[] = [
     {
         method: METHOD.GET as keyof Router,
         path: PATH.PRODUCTS,
+        middleware: [verifyToken],
         handler: productController.getAllProducts
     },
     {
         method: METHOD.GET as keyof Router,
         path: PATH.PRODUCT_ID,
+        middleware: [verifyToken],
         handler: productController.getOneProduct,
     },
     {
         method: METHOD.POST as keyof Router,
         path: PATH.PRODUCTS,
+        middleware: [verifyToken],
         handler: productController.createProduct,
     },
     {
         method: METHOD.PATCH as keyof Router,
         path: PATH.EDIT_PRODUCT_ID,
+        middleware: [verifyToken],
         handler: productController.updateProduct,
     },
     {
         method: METHOD.DELETE as keyof Router,
         path: PATH.PRODUCT_ID,
+        middleware: [verifyToken],
         handler: productController.deleteProduct
     }
 ]
 
 productRoutes.forEach((route) => {
-    const { method, path, handler } = route;
+    const { method, path, middleware = [], handler } = route;
     router[method as any](path, handler);
 });
 
