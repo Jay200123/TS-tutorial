f3577805e3d3641b88ce9f7da8ac7c9b03aedc28
Jay200123 <kenken.ayonon@gmail.com>
2024-11-03 21:53:06 +0800

    Add uploadImage & cloudinary delete image

--- project/route/product/controller.ts
+++ project/route/product/controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "../../interface";
 import { ErrorHandler, SuccessHandler } from "../../utils";
 import * as productService from "../product/service";
+import { uploadImage } from "../../utils";
+import { cloudinary } from '../../config';
 
 const getAllProducts = async (req: Request, res: Response, next: NextFunction) => {
     const data = await productService.getAll();
@@ -15,9 +17,11 @@ const getOneProduct = async (req: Request, res: Response, next: NextFunction) =>
 }
 
 const createProduct = async (req: Request, res: Response, next: NextFunction) => {
+    const image = await uploadImage(req.files as Express.Multer.File[], []);
     const data = await productService.create(
         {
-            ...req.body
+            ...req.body,
+            image: image
         }
     );
 
@@ -25,10 +29,15 @@ const createProduct = async (req: Request, res: Response, next: NextFunction) =>
 }
 
 const updateProduct = async (req: Request, res: Response, next: NextFunction) => {
+    const product = await productService.getById(req.params.id)
+    const oldImage = Array.isArray(product?.image) ? product.image.map((i) => i?.public_id) : [];
+
+    const image = await uploadImage(req.files as Express.Multer.File[], oldImage);
     const data = await productService.updateById(
         req.params.id,
         {
-            ...req.body
+            ...req.body,
+            image: image
         }
     );
 
@@ -37,6 +46,8 @@ const updateProduct = async (req: Request, res: Response, next: NextFunction) =>
 
 const deleteProduct = async (req: Request, res: Response, next: NextFunction) => {
     const data = await productService.deletById(req.params.id);
+    const productImage = Array.isArray(data?.image) ? data.image.map((i) => i?.public_id) : [];
+    await cloudinary.api.delete_resources(productImage)
     return !data ? new ErrorHandler("Product not found") : SuccessHandler(res, "Product deleted successfully", data);
 }
 
